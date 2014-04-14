910a13e792fca19a08b60279aac94637203c315f
xurui203 <xurui203@gmail.com>
2014-04-14 01:06:09 -0400

    readied scrapbook

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/scrapbook', to: 'scrapbook#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
